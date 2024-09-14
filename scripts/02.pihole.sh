52b3dc3b1e98480a6c090718bfffc9c7b2fabc06
ginocic <64708315+ginocic@users.noreply.github.com>
2024-09-14 09:52:08 +0200

    Update and rename pihole.sh to 02.pihole.sh

---
+++ scripts/02.pihole.sh
@@ -0,0 +1,24 @@
+if [ -f /usr/local/bin/pihole ]; then
+  netdevs() {
+    nmap -sP 172.18.12.* | grep report > /tmp/last
+    read -p \'Connect new device then press ENTER\'
+    nmap -sP 172.18.12.* | grep report > /tmp/now
+    diff /tmp/last /tmp/now
+  }
+
+  phuprohi() {
+    wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
+  }
+  
+  alias phrepair="pihole -r"
+  alias phstatus="pihole status"
+  alias phresdns="pihole restartdns"
+  alias phup="pihole -up"
+  alias phchrono="pihole -c"
+  alias phg="pihole -g"
+  
+  echo "        Pi-Hole : phstatus    | phchrono   | phresdns"
+  echo "                  phup        | phg        | phrepair"
+  echo "                  netdevs     | phuprohi"
+  echo ""
+fi
