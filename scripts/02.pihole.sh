e490f2ace74eca7cd2423d0e47b5e4e9dbfeaf4b
ginocic <64708315+ginocic@users.noreply.github.com>
2024-09-14 09:53:09 +0200

    Update 02.pihole.sh

--- scripts/02.pihole.sh
+++ scripts/02.pihole.sh
@@ -17,6 +17,7 @@ if [ -f /usr/local/bin/pihole ]; then
   alias phchrono="pihole -c"
   alias phg="pihole -g"
   
+  echo ""
   echo "        Pi-Hole : phstatus    | phchrono   | phresdns"
   echo "                  phup        | phg        | phrepair"
   echo "                  netdevs     | phuprohi"
